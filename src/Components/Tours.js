cb0aaa646401f1bccd67f935650d3a063d0f6d18
nikhilkumar90 <nikhildevra1921@gmail.com>
2024-08-18 21:41:58 +0530

    add all files

---
+++ src/Components/Tours.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from './Card';
+
+const Tours = ({tours, removeTour}) => {
+  return (
+    <div className='container'>
+        <div>
+            <h2 className='title'>Plan With Love</h2>
+        </div>
+        <div className='cards'>
+            {
+                 tours.map((tour) => {
+                    return <Card {...tour} removeTour = {removeTour}></Card>
+                })
+            }
+        </div> 
+    </div>
+  );
+}
+ 
+export default Tours
